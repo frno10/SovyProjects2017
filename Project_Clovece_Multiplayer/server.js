b9cdd4a22106932dd33893032448996963a39cdb
Michal Lukac <michal.lukac@globallogic.com>
2017-12-18 14:25:33 +0100

    Updated port to be optionally set by Heroku service

--- Project_Clovece_Multiplayer/server.js
+++ Project_Clovece_Multiplayer/server.js
@@ -7,7 +7,7 @@ app.get('/',function(req, res) {
 });
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
 
