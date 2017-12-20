24b76c454126b469a82dd61b5580019d2d32f166
Erik Ruszinka <erik.ruszinka@gmail.com>
2017-12-20 22:28:09 +0200

    changes in Login

--- Project_Clovece_Multiplayer/client/script/preloader.js
+++ Project_Clovece_Multiplayer/client/script/preloader.js
@@ -9,26 +9,3 @@ document.body.onload=function(){
     }, 2000);
     document.getElementById("login").style.boxShadow="13px 13px 27px 4px rgba(0,0,0,0.75)";
 }
-
-function inputOperation(){
-        var firstNum = document.getElementById('myName');
-        var secondNum = document.getElementById('myGame');
-        var error=0;
-        if(firstNum.value=='')
-        {
-        
-        document.getElementById('errorFirst').innerHTML="Invalid input!";
-        }
-        else
-        {
-            document.getElementById('errorFirst').innerHTML="";
-        }
-        if(secondNum.value=='')
-            {
-                document.getElementById('errorSecond').innerHTML="Invalid input!";
-            }
-        else
-            {
-                document.getElementById('errorSecond').innerHTML="";
-            }
-     }
